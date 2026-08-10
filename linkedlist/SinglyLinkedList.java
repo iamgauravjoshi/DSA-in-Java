168dea52ae93e3c9fa038906769f7baeafe3476b
Gaurav Joshi <joshigaurav1122@gmail.com>
2026-08-10 10:06:59 +0530

    Singly LinkedList

--- linkedlist/SinglyLinkedList.java
+++ linkedlist/SinglyLinkedList.java
@@ -1,4 +1,4 @@
-public class SinglyLinkedList {
+class SinglyLinkedList {
     
     private Node head;
     private Node tail;
@@ -21,6 +21,7 @@ public class SinglyLinkedList {
         this.tail = null;
         this.size = 0;
     }
+
 
     // ====================
     //      INSERTION 
@@ -71,17 +72,267 @@ public class SinglyLinkedList {
         size++;
     }
 
+
     // ====================
     //      TRAVERSAL 
     // ====================
     public void printList() {
         Node current = head;
-        while(current.next != null) {
-            System.out.println(current.data);
+        while(current != null) {
+            System.out.print(current.data + " ");
             current = current.next;
         }
+        System.out.println();
     }
-    public static void main(String[] args) {
+
+
+    // ====================
+    //  UTILITY FUNCTIONS 
+    // ====================
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int getHead() {
+        if(head == null) {
+            return -1;
+        } else {
+            return head.data;
+        }
+    }
+
+    public int getTail() {
+        if(tail == null) {
+            return -1;
+        } else {
+            return tail.data;
+        }
+    }
+
+    public void clear() {
+        if(head == null) {
+            System.out.println("LinkedList is already empty");
+        } else {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+    }
+ 
+    
+    // ====================
+    //      SEARCHING 
+    // ====================
+    public boolean search(int target) {
+        Node current = head;
+        while(current != null) {
+            if(current.data == target) {
+                return true;
+            } else {
+                current = current.next;
+            }
+        }
+        return false;
+    }
+
+    public int findPosition(int target) {
+        Node current = head;
+        int index = 1;
+        while(current != null) {
+            if(current.data == target) {
+                return index;
+            } else {
+                current = current.next;
+                index++;
+            }
+        }
+        return -1;
+    }
+
+
+    // ====================
+    //      UPDATION 
+    // ====================
+    // Update value using position
+    public void updateAtPosition(int position, int newData) {
+        if(position < 1 || position > size) {
+            System.out.println("IndexOutOfBound. Given index " + position + " not found!");
+            return;
+        }
+        Node current = head; 
+        for(int i=1; i<=position; i++) {
+            if(i == position) {
+                current.data = newData;
+                return;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+    // Update first occurance of value
+    public boolean updateValue(int oldValue, int newValue) {
+        Node current = head;
+        while(current != null) {
+            if(current.data == oldValue) {
+                current.data = newValue;
+                return true;
+            } else {
+                current = current.next;
+            }
+        }
+        System.out.println("Node not found with value " + oldValue);
+        return false;
+    }
+
+    // ====================
+    //      DELETION 
+    // ====================
+    public void deleteHead() {
+        if(head == null) {
+            System.out.println("LinkedList is already empty");
+            return;
+        } else if(head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+        size--;
+    }
+
+    public void deleteTail() {
+        if(head == null) {
+            System.out.println("LinkedList is already empty");
+            return;
+        } else if(head == tail) {
+            head = tail = null;
+        } else {
+            Node current = head;
+            for(int i=1; i<size-1; i++) {
+                current = current.next;
+            }
+            current.next = null;
+            tail = current;
+        }
+        size--;
+    }
+
+    public void deleteAtPosition(int position) {
+        if(position < 1 || position > size) {
+            System.out.println("IndexOutOfBound. Given index " + position + " not found!");
+            return;
+        } else if(position == 1) {
+            deleteHead();
+            return;
+        } else if(position == size) {
+            deleteTail();
+            return;
+        } else {
+            Node current = head;
+            for(int i=1; i<position; i++) {
+                current = current.next;
+            }
+            Node nodeToDelete = current.next;
+            current.next = nodeToDelete.next;
+            nodeToDelete.next = null;
+            size--;
+        }
+    }
+
+    public void deleteValue() {
         
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        // if(list.isEmpty()) {
+        //     System.out.println("List is empty");
+        // }
+
+        // System.out.println("Size of LL: " + list.getSize());
+        // list.insertAtHead(10);
+        // list.printList();
+        // list.insertAtHead (20);
+        // list.printList();
+        // list. insertAtHead (30);
+        // list.printList();
+        // list.insertAtTail(100);
+        // list.printList();
+        // list. insertAtTail(110);
+        // list.printList();
+        // list.insertAtTail(120);
+        // list.printList();
+
+        // list.insertAtPosition(1, 1);
+        // list.printList();
+
+        // list.insertAtPosition(10, 7);
+        // list.insertAtPosition(7, 7);
+        // list.printList();
+
+        // System.out.println("Is 20 exists: " + list.search(20));
+        // System.out.println("Node 110 position: " + list.findPosition(110));
+
+        // list.updateAtPosition(8, 8);
+        // list.updateValue(15, 25);
+        // list.printList();
+
+        // list.clear();
+        // list.printList();
+        
+        list.insertAtHead(10);
+        list.insertAtTail(20);
+        list.insertAtTail(30);
+        list.printList();
+
+        // System.out.println("----- Head Deletion -----");
+
+        // System.out.println("head data: " + list.getHead());
+        // System.out.println("tail data: " + list.getTail());
+        // System.out.println("list size: " + list.getSize());
+
+        // list.deleteHead();
+        // System.out.println();
+
+        // System.out.println("head data: " + list.getHead());
+        // System.out.println("tail data: " + list.getTail());
+        // System.out.println("list size: " + list.getSize());
+
+        // list.deleteHead();
+        // list.printList();
+        // System.out.println();
+
+        // System.out.println("head data: " + list.getHead());
+        // System.out.println("tail data: " + list.getTail());
+        // System.out.println("list size: " + list.getSize());
+
+        // System.out.println("----- Tail Deletion -----");
+
+        // System.out.println("head data: " + list.getHead());
+        // System.out.println("tail data: " + list.getTail());
+        // System.out.println("list size: " + list.getSize());
+
+        // list.deleteTail();
+        // System.out.println();
+
+        // System.out.println("head data: " + list.getHead());
+        // System.out.println("tail data: " + list.getTail());
+        // System.out.println("list size: " + list.getSize());
+
+        // list.deleteTail();
+        // list.printList();
+        // System.out.println();
+
+        // System.out.println("head data: " + list.getHead());
+        // System.out.println("tail data: " + list.getTail());
+        // System.out.println("list size: " + list.getSize());
+
+        list.deleteAtPosition(2);
+        list.printList();
     }
 }
