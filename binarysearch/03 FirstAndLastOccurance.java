5787d6235cfa4769a7a9b2a21c39a8c4609ed923
Gaurav Joshi <joshigaurav1122@gmail.com>
2026-08-11 15:55:12 +0530

    Binary search DSA

--- binarysearch/03 FirstAndLastOccurance.java
+++ binarysearch/03 FirstAndLastOccurance.java
@@ -1,58 +1,58 @@
+
 import java.util.Arrays;
 
 class FirstAndLastOccurrence {
-  public static void main(String[] args) {
-    int[] arr1 = {1, 3, 5, 5, 5, 5, 67, 125, 125};
-    int[] arr2 = {};
-    int target1 = 5;
-    int target2 = 67;
-    int target3 = 0;
 
-    int[] result1 = firstAndLastOccurrence(arr1, target1);
-    int[] result2 = firstAndLastOccurrence(arr1, target2);
-    int[] result3 = firstAndLastOccurrence(arr2, target3);
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 5, 5, 5, 67, 125, 125};
+        int[] arr2 = {};
+        int target1 = 5;
+        int target2 = 67;
+        int target3 = 0;
 
-    System.out.println("First and Last Occurrence of " + target1 + ": " + Arrays.toString(result1));
-    System.out.println("First and Last Occurrence of " + target2 + ": " + Arrays.toString(result2));
-    System.out.println("First and Last Occurrence of " + target3 + ": " + Arrays.toString(result3));
-  }
+        int[] result1 = firstAndLastOccurrence(arr1, target1);
+        int[] result2 = firstAndLastOccurrence(arr1, target2);
+        int[] result3 = firstAndLastOccurrence(arr2, target3);
 
-  static int[] firstAndLastOccurrence(int arr[], int target) {
-
-    int first = findOccurrence(arr, target, true);
-    int last = findOccurrence(arr, target, false);
-
-    return new int[]{first, last};
-  }
-
-  static int findOccurrence(int[] arr, int target, boolean findFirst) {
-    if(arr == null || arr.length == 0) {
-      return -1;
+        System.out.println("First and Last Occurrence of " + target1 + ": " + Arrays.toString(result1));
+        System.out.println("First and Last Occurrence of " + target2 + ": " + Arrays.toString(result2));
+        System.out.println("First and Last Occurrence of " + target3 + ": " + Arrays.toString(result3));
     }
 
-    int start = 0;
-    int end = arr.length-1;
-    int ans = -1;
+    static int[] firstAndLastOccurrence(int arr[], int target) {
 
-    while(start <= end) {
-      int mid = start + (end-start)/2;
+        int first = findOccurrence(arr, target, true);
+        int last = findOccurrence(arr, target, false);
 
-      if(arr[mid] == target) {
-        ans = mid;
+        return new int[]{first, last};
+    }
 
-        if(findFirst) {
-          end = mid-1;
-        } else {
-          start = mid+1;
+    static int findOccurrence(int[] arr, int target, boolean findFirst) {
+        if (arr == null || arr.length == 0) {
+            return -1;
         }
-      } else if(target < arr[mid]) {
-        end = mid-1;
-      } else {
-        start = mid+1;
-      }
-    }
-    return ans;
-  }
-}  
 
-  
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+
+                if (findFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
--- binarysearch/04 CountElement.java
+++ binarysearch/04 CountElement.java
@@ -1,4 +1,6 @@
+
 class CountElementInSortedArray {
+
     public static void main(String[] args) {
         int[] arr1 = {1, 3, 5, 5, 5, 5, 67, 125, 125};
         int[] arr2 = {4, 4, 8, 8, 8, 15, 16, 23, 23, 42};
@@ -25,31 +27,80 @@ class CountElementInSortedArray {
     }
 
     static int findOccurrence(int[] arr, int target, boolean findFirst) {
-        if(arr == null || arr.length == 0) {
+        if (arr == null || arr.length == 0) {
             return 0;
         }
 
         int start = 0;
-        int end = arr.length-1;
+        int end = arr.length - 1;
         int ans = -1;
 
-        while(start <= end) {
-            int mid = start + (end-start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if(arr[mid] == target) {
+            if (arr[mid] == target) {
                 ans = mid;
 
-                if(findFirst) {
-                end = mid-1;
+                if (findFirst) {
+                    end = mid - 1;
                 } else {
-                start = mid+1;
+                    start = mid + 1;
                 }
-            } else if(target < arr[mid]) {
-                end = mid-1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
             } else {
-                start = mid+1;
+                start = mid + 1;
             }
         }
         return ans;
+    }
+}
+
+class Solution {
+
+    static int countFreq(int arr[], int target) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+        int firstIndex = -1;
+        int lastIndex = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                firstIndex = mid;
+                end = mid - 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        start = 0;
+        end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                lastIndex = mid;
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        if (firstIndex == -1) {
+            firstIndex = 0;
+        }
+
+        return lastIndex - firstIndex + 1;
     }
 }
