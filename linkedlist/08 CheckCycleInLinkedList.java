c0e6ba767039907748b79dfa74fe42a4fb7e282b
Gaurav Joshi <joshigaurav1122@gmail.com>
2026-08-19 06:20:19 +0530

    Solved: Find start of cycle in LinkedList

--- linkedlist/08 CheckCycleInLinkedList.java
+++ linkedlist/08 CheckCycleInLinkedList.java
@@ -28,9 +28,48 @@ Explanation: There is no cycle in the linked list.
     -> pos is -1 or a valid index in the linked-list.
  */
 
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
 class CheckCycleInLinkedList {
 
     public static void main(String[] args) {
+        CheckCycleInLinkedList s = new CheckCycleInLinkedList();
+        Node head = new Node(1);
 
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        // Create cycle: last node points back to node containing 2
+        head.next.next.next.next.next = head.next.next;
+
+        System.out.println("Cycle detected: " + s.hasCycle(head));
+    }
+
+    public boolean hasCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
